5fb4f61cd27acb5f32f45b4219163dc7d3d1258c
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-03-29 00:03:43 +0500

    TRYING TO FIX

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import mdx from "@mdx-js/rollup";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import image from "@rollup/plugin-image";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [mdx(), tsconfigPaths(), remix()],
-  assetsInclude: ["**/*.svg", "**/*.png", "**/*.jpg", "**/*.ttf"],
+  plugins: [{ ...image(), enforce: "pre" }, mdx(), tsconfigPaths(), remix()],
 });
