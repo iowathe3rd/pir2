613cb73f1e278e3e3bea76298c2cfe543817148e
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-03-29 00:09:21 +0500

    rollup bug

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,25 @@ import mdx from "@mdx-js/rollup";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import image from "@rollup/plugin-image";
+import path from "node:path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [{ ...image(), enforce: "pre" }, mdx(), tsconfigPaths(), remix()],
+  plugins: [mdx(), tsconfigPaths(), { ...image(), enforce: "pre" }, remix()],
+  resolve: {
+    alias: [
+      {
+        find: "~",
+        replacement: path.resolve(__dirname, "app"),
+      },
+    ],
+  },
+  build: {
+    rollupOptions: {
+      plugins: image(),
+    },
+  },
 });
